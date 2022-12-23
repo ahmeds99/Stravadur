29b3f9547bc7214c6c0c703d32fdcaf522530515
Ahmed Waseem Saeed <ahmedws@uio.no>
2022-12-23 23:18:44 +0100

    Add Activity (and Map) interface

--- src/api/types.tsx
+++ src/api/types.tsx
@@ -7,3 +7,40 @@ export interface Athlete {
   country?: any;
   createdAt: Date;
 }
+
+export interface Map {
+  id: string;
+  summaryPolyline: string;
+  resourceState: number;
+}
+
+export interface Activity {
+  name: string;
+  distance: number;
+  movingTime: number;
+  elapsedTime: number;
+  totalElevationGain: number;
+  type: string;
+  sportType: string;
+  workoutType: number;
+  id: any;
+  startDate: Date;
+  startDateLocal: Date;
+  locationCity?: any;
+  locationState?: any;
+  locationCountry?: any;
+  achievementCount: number;
+  kudosCount: number;
+  commentCount: number;
+  athleteCount: number;
+  photoCount: number;
+  map: Map;
+  manual: boolean;
+  private: boolean;
+  visibility: string;
+  flagged: boolean;
+  averageSpeed: number;
+  maxSpeed: number;
+  prCount: number;
+  totalPhotoCount: number;
+}
