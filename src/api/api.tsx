3c707d75dd74f7b74d7cffe0c2f2f1226921bb26
Ahmed Waseem Saeed <ahmedws@uio.no>
2022-12-23 20:59:00 +0100

    Add API-calls to fetch athlete and activities

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,25 +1,39 @@
 import { Athlete } from "./types";
 
-export async function getAthlete(
-  accessToken: string,
-  setAthlete: (state: Athlete) => void
-) {
-  console.log(accessToken);
+export async function getActivities(accessToken: string) {
+  const response = await fetch(
+    "https://www.strava.com/api/v3/athlete/activities",
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
 
+  return await response.json();
+}
+
+export async function getAthlete(accessToken: string): Promise<Athlete | null> {
   const response = await fetch("https://www.strava.com/api/v3/athlete", {
     method: "GET",
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      return data as Athlete;
-    });
+  });
 
-  //   console.log(response);
-
-  //   setAthlete(response);
-
-  //   return response
+  if (!response.ok) return null;
+  return mapAthlete(await response.json());
 }
+
+const mapAthlete = (data: any): Athlete => {
+  return {
+    id: data.id,
+    username: data.username,
+    firstname: data.firstname,
+    lastname: data.lastname,
+    city: data.city,
+    country: data.country,
+    createdAt: data.created_at,
+  };
+};
